85a79fd762ac81731d06f2f7bcccef05b8ac3050
Tem4ik91 <purgen-666@ya.ru>
2021-10-19 15:51:43 +0300

    Initial commit

---
+++ src/GameField.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class GameField extends JPanel {
+    private final int SIZE = 320;
+    private final int DOT_SIZE = 16;
+    private final int ALL_DOTS = 16;
+
+    public GameField(){
+
+    }
+
+
+
+}
---
+++ src/Window.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+public class Window extends JFrame {
+    public Window(){
+        setTitle("Змейка");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(320,345);
+        setLocation(400,400);
+        add(new GameField());
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+
+        Window mw = new Window();
+
+    }
+}
